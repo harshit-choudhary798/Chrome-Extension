50cec2ab1ab71287aed9c61785d762854c71a680
Harshit Choudhary <harshit1840077@gmail.com>
2023-02-07 18:30:59 +0530

    commit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-
+let count=0
 inputBtn.addEventListener("click", function() {
+
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    for (let i = count; i < myLeads.length; i++) {
+        ulEl.innerHTML+= "<li>" + myLeads[i]+" " + "</li>"
+    }
+   count++;
 })
 
 
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-}
+
+
+
+
+
 
 
 
