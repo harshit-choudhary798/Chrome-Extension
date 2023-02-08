68bf4bdd6fe92998d8926d0bb476491a00cb484d
Harshit Choudhary <harshit1840077@gmail.com>
2023-02-08 11:34:14 +0530

    commit

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ let myLeads =[]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
+
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     
     myLeads.push(inputEl.value)
-    renderLeads()
     inputEl.value=" "
+    //SAVE MY LEADS ARRAY TO LOCALSTORAGE
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads()
+    //VERIFY IF IT WORKS
+    console.log(localStorage.getItem("myLeads"))
 })
+
+
+
+
+
 
 function renderLeads(){
     let listItems=" "
@@ -23,7 +49,6 @@ function renderLeads(){
     ulEl.innerHTML=listItems
     
 }
-
 
 
 
